1427b06cfd5b29c383a9a77784802821dc7d7ec1
Alexis BALTHAZARD <alexis31.balthazard@outlook.fr>
2020-04-14 18:18:04 +0200

    Embed correction

--- commands/profile.js
+++ commands/profile.js
@@ -1,4 +1,6 @@
-﻿exports.run = (client, message, args) => {
+﻿const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
 
 	let user = (
 		message.mentions.users.size ? message.mentions.users.first() : null
@@ -22,51 +24,46 @@
 		nick = user.username;
 	}
 
-	message.channel.send({
-		embed: {
-			color: 0x00AE86,
-			author: {
-				name: `Profil de ${user.username}`
-			},
-			description: "```Voici le profil recherché !```",
-			thumbnail: {
-				url: user.displayAvatarURL({
-					format: 'png',
-					dynamic: true,
-					size: 128
-				})
-			},
-			fields: [{
-					"name": "__**Discord** :__",
-					"value": user.tag,
-					"inline": true
-				},
-				{
-					"name": "__**Pseudo Warframe** :__",
-					"value": nick,
-					"inline": true
-				},
-				{
-					"name": "__**Palier actuel** :__",
-					"value": `**${client.config.rank[userinfo.rank].name}** ( ${userinfo.rank}${paliemoji} - ${userinfo.points}points )`,
-					"inline": true
-				},
-				{
-					"name": "__**As rejoins le Discord le** :__",
-					"value": `${timestamp.toLocaleString('fr-FR').slice(0, 20)}`,
-					"inline": true
-				}
-			],
-			timestamp: user.createdTimestamp,
-			footer: {
-				icon_url: client.user.displayAvatarURL({
-					format: 'png',
-					dynamic: true,
-					size: 128
-				}),
-				text: `Servis pour vous par Rafiki alias ${client.users.cache.get("207128825895714816").tag} | ${Math.pow((userinfo.rank + 1) / 0.1, 2) - userinfo.points} points avant le prochain palier !`
+	let profileEmbed = new Discord.MessageEmbed()
+		.setColor(0x00AE86)
+		.setAuthor(`Profil de ${user}`)
+		.setDescription("```Voici le profil recherché !```")
+		.setThumbnail(user.displayAvatarURL({
+			format: 'png',
+			dynamic: true,
+			size: 128
+		}))
+		.setTimestamp(user.createdTimestamp)
+		.addFields([
+			{
+				name: "__**Discord** :__",
+				value: user.tag,
+				inline: true
+			}, {
+				name: "__**Pseudo Warframe** :__",
+				value: nick,
+				inline: true
+			}, {
+				name: "__**Palier actuel** :__",
+				value: `**${client.config.rank[userinfo.rank].name}** ( ${userinfo.rank}${paliemoji} - ${userinfo.points}points )`,
+				inline: true
+			}, {
+				name: "__**As rejoins le Discord le** :__",
+				value: `${timestamp.toLocaleString('fr-FR').slice(0, 20)}`,
+				inline: true
 			}
-		}
+		])
+		.setFooter(
+			`Servis pour vous par Rafiki alias ${client.users.cache.get("207128825895714816").tag} | ${Math.pow((userinfo.rank + 1) / 0.1, 2) - userinfo.points} points avant le prochain palier !`,
+			client.user.displayAvatarURL({
+				format: 'png',
+				dynamic: true,
+				size: 128
+			})
+		);
+
+	message.channel.send({
+		embed: profileEmbed
 	});
 };
 
